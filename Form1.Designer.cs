bb606daa2c52bfcd36c95186cc253c81823e9818
Abdelrahmannfayez <abdelrahmannfayez@gmail.com>
2023-02-22 10:53:36 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿
+namespace plagiarism_checker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel6.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel10.SuspendLayout();
+            this.panel11.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.Location = new System.Drawing.Point(3, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(317, 78);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Read File One";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button2.Location = new System.Drawing.Point(3, 299);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(317, 87);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Read File Two";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(43, 51);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(391, 363);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Location = new System.Drawing.Point(479, 51);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(409, 363);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button3.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button3.Location = new System.Drawing.Point(3, 660);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(341, 96);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Check Similarity";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F);
+            this.label2.Location = new System.Drawing.Point(59, 520);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 54);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(54)))), ((int)(((byte)(78)))));
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(-12, -32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(320, 774);
+            this.panel1.TabIndex = 7;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button5.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button5.Location = new System.Drawing.Point(3, 417);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(317, 87);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Clear File One";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button4.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.button4.Location = new System.Drawing.Point(3, 551);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(317, 87);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Clear File Two";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.panel6);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Location = new System.Drawing.Point(3, 28);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(317, 117);
+            this.panel2.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::plagiarism_checker.Properties.Resources.opacity;
+            this.pictureBox1.Location = new System.Drawing.Point(111, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 79);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.panel6.Controls.Add(this.panel7);
+            this.panel6.Controls.Add(this.panel9);
+            this.panel6.Location = new System.Drawing.Point(321, 8);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(919, 117);
+            this.panel6.TabIndex = 11;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.panel7.Controls.Add(this.panel8);
+            this.panel7.Location = new System.Drawing.Point(321, 8);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(919, 117);
+            this.panel7.TabIndex = 10;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.panel8.Location = new System.Drawing.Point(321, 3);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(919, 117);
+            this.panel8.TabIndex = 9;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.panel9.Location = new System.Drawing.Point(321, 3);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(919, 117);
+            this.panel9.TabIndex = 9;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.panel4.Controls.Add(this.panel5);
+            this.panel4.Location = new System.Drawing.Point(321, 8);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(919, 117);
+            this.panel4.TabIndex = 10;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.panel5.Location = new System.Drawing.Point(321, 3);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(919, 117);
+            this.panel5.TabIndex = 9;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(58)))));
+            this.panel3.Location = new System.Drawing.Point(321, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(919, 117);
+            this.panel3.TabIndex = 9;
+            // 
+            // panel10
+            // 
+            this.panel10.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.panel10.Controls.Add(this.label3);
+            this.panel10.Location = new System.Drawing.Point(308, -29);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(926, 147);
+            this.panel10.TabIndex = 8;
+            this.panel10.Paint += new System.Windows.Forms.PaintEventHandler(this.panel10_Paint);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.Location = new System.Drawing.Point(218, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(456, 46);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Text-Similarity Checker";
+            // 
+            // panel11
+            // 
+            this.panel11.BackColor = System.Drawing.Color.White;
+            this.panel11.Controls.Add(this.richTextBox2);
+            this.panel11.Controls.Add(this.richTextBox1);
+            this.panel11.Controls.Add(this.label2);
+            this.panel11.Location = new System.Drawing.Point(308, 108);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(979, 616);
+            this.panel11.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1228, 714);
+            this.Controls.Add(this.panel11);
+            this.Controls.Add(this.panel10);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel6.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel10.ResumeLayout(false);
+            this.panel10.PerformLayout();
+            this.panel11.ResumeLayout(false);
+            this.panel11.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel11;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace plagiarism_checker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Hide();
+        }
+
+        //[1]inserting first file
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog() { Filter = "Text Documents.txt|*", ValidateNames = true, Multiselect = false })
+                if ( ofd.ShowDialog()==DialogResult.OK)
+                {
+                    using (StreamReader sr = new StreamReader(ofd.FileName))
+                        
+                    richTextBox1.Text = sr.ReadToEnd();
+
+                }
+        }
+       
+        //[2]insering  second file
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd2 = new OpenFileDialog() { Filter = "Text Documents.txt|*", ValidateNames = true, Multiselect = false })
+                if (ofd2.ShowDialog() == DialogResult.OK)
+                {
+                    using (StreamReader sr = new StreamReader(ofd2.FileName))
+
+                        richTextBox2.Text = sr.ReadToEnd();
+
+                }
+        }
+        //comaparing beween both text files
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            
+            //label2.Text = "Similaty checker takes seconds";
+            label2.Show();
+
+            string[] words_one = Regex.Split(richTextBox1.Text, @"\W+");
+            string[] words_two = Regex.Split(richTextBox2.Text, @"\W+");
+
+            words_one = words_one.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+            words_two = words_two.Where(x => !string.IsNullOrEmpty(x)).ToArray();
+
+            Dictionary<string, double> counter1 = new Dictionary<string, double>();
+            Dictionary<string, double> counter2 = new Dictionary<string, double>();
+
+            foreach (string word in words_one)
+            {
+                string x = word.ToLower();
+                if (counter1.TryGetValue(x, out double count))
+                {
+                    if (count < 100000) counter1[x] = count + 1;
+                }
+                else
+                {
+                    counter1[x] = 1;
+                }
+            }
+
+            foreach (string word in words_two)
+            {
+                string x = word.ToLower();
+                if (counter2.TryGetValue(x, out double count))
+                {
+                    if (count < 100000) counter2[x] = count + 1;
+                }
+                else
+                {
+                    counter2[x] = 1;
+                }
+            }
+
+            double dotx = 0;
+            double squaredabsolute1 = 0;
+            double squaredabsolute2 = 0;
+
+            foreach (KeyValuePair<string, double> R in counter1)
+            {
+                if (counter2.TryGetValue(R.Key, out double count))
+                {
+                    dotx += R.Value * count;
+                }
+                squaredabsolute1 += R.Value * R.Value;
+            }
+
+            foreach (KeyValuePair<string, double> R in counter2)
+            {
+                squaredabsolute2 += R.Value * R.Value;
+            }
+
+            double cosangle = dotx / Math.Sqrt(squaredabsolute1 * squaredabsolute2);
+            double angle = Math.Acos(cosangle);
+            angle = angle * 180 / Math.PI;
+            angle = 90 - angle;
+            angle = (angle / 90) * 100;
+            angle = (int)angle;
+            if (angle < 0) label2.Text = "Please Insert Both Text Files";
+            else {
+                string theta = angle.ToString();
+                if (theta == "Nan") label2.Text = "Please Insert Both Text Files";
+                else
+                label2.Text = theta+'%';
+            }
+            
+
+            
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = " ";
+            label2.Text = " ";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel10_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text = " ";
+            label2.Text = " ";
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.ReadOnly = true;
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox2.ReadOnly = true;
+        }
+    }
+}
